0247d2a9bbe698de28e78ac47d3c293473c74f8d
Volha Krypulevich <volha_krypulevich@epam.com>
2020-07-23 21:53:56 +0300

    add base view model

--- FitAppTexode/WpfFit/Models/User.cs
+++ FitAppTexode/WpfFit/Models/User.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace WpfFit.Models
 {
     /// <summary>
     /// User information for the application table.
     /// </summary>
-    public class User
+    public class User : BaseVM
     {
         /// <summary>
         /// User full name.
@@ -15,21 +16,22 @@ namespace WpfFit.Models
         /// <summary>
         /// Average number of steps taken over the entire period.
         /// </summary>
-        public int AverageStepsNumber { get; set; }
+        public int AverageStepsNumber { get; private set; }
 
         /// <summary>
         /// The best result for the entire period.
         /// </summary>
-        public int TheBestResult { get; set; }
+        public int TheBestResult { get; private set; }
 
         /// <summary>
         /// The worst result for the entire period.
         /// </summary>
-        public int TheWorstResult { get; set; }
+        public int TheWorstResult { get; private set; }
 
         /// <summary>
         /// User data for the entire period ( <day, user information for a day> ).
         /// </summary>
-        public ICollection<KeyValuePair<int, UserInformationForADay>> UserData { get; set; }
+        public ObservableCollection<KeyValuePair<int, UserInformationForADay>> UserData { get; set; }
+            = new ObservableCollection<KeyValuePair<int, UserInformationForADay>>();
     }
 }
