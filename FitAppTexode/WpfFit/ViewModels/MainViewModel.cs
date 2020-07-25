d536e3c17ea0c350cacae8c006d9fc73a9c9a82c
Volha Krypulevich <volha_krypulevich@epam.com>
2020-07-25 21:31:17 +0300

    add livechart

--- FitAppTexode/WpfFit/ViewModels/MainViewModel.cs
+++ FitAppTexode/WpfFit/ViewModels/MainViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using LiveCharts;
+using LiveCharts.Configurations;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -22,23 +24,46 @@ namespace WpfFit.ViewModels
 {
     public class MainViewModel : BaseVM
     {
-        private User selectedUser;
+        private User _selectedUser;
+        private ChartValues<int> _selectedUserSteps;        
 
-        public NotifyTaskCompletion<IList<User>> Users { get; private set; }
+        public NotifyTaskCompletion<IList<User>> Users { get; set; }
+        public ChartValues<int> Days { get; private set; }
+        //public Brush DangerBrush { get; set; }
+        //public CartesianMapper<ObservableValue> Mapper { get; set; }
+        public User SelectedUser
+        {
+            get { return _selectedUser; }
+            set
+            {
+                _selectedUser = value;
+                var selectedUserSteps = _selectedUser.UserData.Select(u => (u.Value.Steps));
+                SelectedUserSteps = new ChartValues<int>(selectedUserSteps);
+                var days = _selectedUser.UserData.Select(u => (u.Key));
+                Days = new ChartValues<int>(days);
+                OnPropertyChanged("SelectedUser");
+            }
+        }
+        public ChartValues<int> SelectedUserSteps
+        {
+            get { return _selectedUserSteps; }
+            set
+            {
+                _selectedUserSteps = value;
+                OnPropertyChanged("SelectedUserSteps");
+            }
+        }
 
         public MainViewModel(IFileService fileService)
         {
             Users = new NotifyTaskCompletion<IList<User>>(fileService.GetUsersStatistic());
-        }
 
-        public User SelectedUser
-        {
-            get { return selectedUser; }
-            set
-            {
-                selectedUser = value;
-                OnPropertyChanged("SelectedUser");
-            }
+            //Mapper = Mappers.Xy<ObservableValue>()
+            //    .X((item, index) => index)
+            //    .Y(item => item.Value)
+            //    .Fill(item => item.Value > 40000 ? DangerBrush : null)
+            //    .Stroke(item => item.Value > 40000 ? DangerBrush : null);
+            //DangerBrush = new SolidColorBrush(Color.FromRgb(238, 83, 80));
         }
     }
 }
